9d2be8b0ed79371045a094c60cf70939d124d611
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-12-19 10:53:46 +0100

    Add the endpoint /products/:id that Retrieve a product by their unique ID.

--- backend/controllers/ProductsController.js
+++ backend/controllers/ProductsController.js
@@ -1,6 +1,7 @@
 // controllers/ProductsController.js
 
 import Product from '../models/Product.js';
+import mongoose from 'mongoose';
 
 class ProductsController {
 
@@ -9,6 +10,20 @@ class ProductsController {
     try {
       const products = await Product.find();
       return res.json(products);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  static async getProductById(req, res) {
+    const productId = req.params.id;
+
+    try {
+      // Check if the product exists
+      const product = await Product.findById(productId);
+      if (!product) return res.status(404).json({ error: 'Product not found'});
+
+      return res.json(product);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,5 +6,6 @@ import ProductsController from '../controllers/ProductsController.js';
 const router = express.Router();
 
 router.get('/', ProductsController.getProducts);
+router.get('/:id', ProductsController.getProductById);
 
 export default router;
